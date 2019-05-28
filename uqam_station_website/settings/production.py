dd03c85b80f7ad8c03a81ce5097bf35af28ece9a
FX <f.xavier.dueymes@gmail.com>
2019-05-28 11:31:23 -0400

    ajout d'une configuration de docker

---
+++ uqam_station_website/settings/production.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = False
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+SESSION_COOKIE_SECURE = True
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
