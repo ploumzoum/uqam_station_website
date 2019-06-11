9d6f85025ac8ff80bc18f9fcdb15e0f704a58c93
FX <f.xavier.dueymes@gmail.com>
2019-06-11 16:16:53 -0400

    affinage des options des datespickers

--- apps/data_form/forms.py
+++ apps/data_form/forms.py
@@ -2,16 +2,17 @@ from bootstrap_datepicker_plus import DatePickerInput, TimePickerInput
 from django import forms
 
 
-MONTHS = {
-    1:'Janvier', 2:'Février', 3: 'Mars', 4: 'Avril',
-    5: 'Mai ', 6: 'Juin', 7: 'Juillet', 8: 'Août',
-    9: 'Septembre', 10: 'Octobre', 11: 'Novembre', 12: 'Décembre'
-}
 class SingleDateForm(forms.Form):
     date = forms.DateField(
         label='Date',
         required=True,
-        widget=DatePickerInput())
+        widget=DatePickerInput(
+            options={
+                'format': '%d-%m-%Y',
+                'minDate': '01/01/2018'
+            }
+        )
+    )
     data_type = forms.MultipleChoiceField(label='Type de données', choices=(('instant', 'Instantané'), ('average', 'Moyenne')))
     time = forms.TimeField(
         label='Heure et minute',
