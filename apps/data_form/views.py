67449fd9476d5c971582eb38515ffed7c9f2b045
FX <f.xavier.dueymes@gmail.com>
2019-06-13 16:31:27 -0400

    ajout des methodes pour creer des dataframes

--- apps/data_form/views.py
+++ apps/data_form/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import HttpResponseRedirect
+import csv
+
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 
 from controllers.weather_db import get_entry_with_variables
@@ -27,3 +29,19 @@ def data_form(request):
         form = SingleDateForm()
 
     return render(request, 'data_form/data_form.html', {'form': form})
+
+
+class Echo:
+    """An object that implements just the write method of the file-like
+    interface.
+    """
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
+
+
+def download_csv(data):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
+
+    writer = csv.writer(response)
--- controllers/weather_db.py
+++ controllers/weather_db.py
@@ -1,26 +1,25 @@
-from datetime import datetime
 from django.db import OperationalError, connections
 import wrapt
 from models.weather_data import WeatherData
 
 
 @wrapt.decorator
-def check_connection(func, *args):
+def check_connection(wrapped, instance=None, args=None, kwargs=None):
     try:
-        return func(*args)
+        return wrapped(*args)
     except OperationalError:
         conn = connections['weather_data']
         conn.connect()
-        return func(*args)
+        return wrapped(*args)
 
 
 @check_connection
-def get_first_entry(*args):
+def get_first_entry():
     return WeatherData.objects.order_by('date').first()
 
 
 @check_connection
-def get_last_entry(*args):
+def get_last_entry():
     return WeatherData.objects.last()
 
 
