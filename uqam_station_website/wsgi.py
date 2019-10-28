b32dd07fade2587945e1c3a4e62fb13a92d0e69c
Francois-Xavier <f.xavier.dueymes@gmail.com>
2019-10-28 16:00:02 -0400

    fix de wsgi"

--- uqam_station_website/wsgi.py
+++ uqam_station_website/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uqam_station_website.nginx.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uqam_station_website.settings.production')
 
 application = get_wsgi_application()
